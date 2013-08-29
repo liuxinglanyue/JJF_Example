66386ede00d8565bfcc5caff5def89d41d457e69
jianfeng.jiao <jianfeng.jiao@sihuatech.com>
2013-08-29 14:04:02 +0800

    Base64 例子

---
+++ src/com/decode/Base64Test.java
@@ -0,0 +1,40 @@
+package com.decode;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * Base64 例子
+ * @author Jiao JianFeng
+ *
+ */
+public class Base64Test {
+	public static void main(String[] args) {
+		String imageInfo = "liuxinglanyue";
+		System.out.println(imageInfo = encodeStr(imageInfo));
+		System.out.println(decodeStr(imageInfo));
+	}
+	
+	/**
+	 * base64 编码
+	 * @param text
+	 * @return
+	 */
+	public static String encodeStr(String text) {
+		byte[] b = text.getBytes();
+		Base64 base64 = new Base64();
+		b = base64.encode(b);
+		return new String(b);
+	}
+	
+	/**
+	 * base64解码
+	 * @param text
+	 * @return
+	 */
+	public static String decodeStr(String text) {
+		byte[] b = text.getBytes();
+		Base64 base64 = new Base64();
+		b = base64.decode(b);
+		return new String(b);
+	}
+}
