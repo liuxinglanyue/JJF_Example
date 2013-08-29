eb60e7a08fb08237bdd5aa87987a8fa4e28ce4a7
jianfeng.jiao <jianfeng.jiao@sihuatech.com>
2013-08-29 14:47:14 +0800

    md5

---
+++ src/com/decode/MD5Test.java
@@ -0,0 +1,34 @@
+package com.decode;
+
+import org.apache.commons.codec.binary.BinaryCodec;
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class MD5Test {
+	public static void main(String[] args) {
+		String text = "我们哥俩好";
+		System.out.println(md5Hex(text));
+		System.out.println(shaHex(text));
+		System.out.println(md5ToBinary(text));
+	}
+	
+	public static String md5Hex(String text) {
+		if(null == text) {
+			return null;
+		}
+		return DigestUtils.md5Hex(text);
+	}
+	
+	public static String shaHex(String text) {
+		if(null == text) {
+			return null;
+		}
+		return DigestUtils.shaHex(text);
+	}
+	
+	public static String md5ToBinary(String text) {
+		if(null == text) {
+			return null;
+		}
+		return new String(BinaryCodec.toAsciiBytes(DigestUtils.md5(text)));
+	}
+}
