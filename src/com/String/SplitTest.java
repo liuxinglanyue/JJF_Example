885cd9b27aaffae37af195d59d0decfa13a98fe1
jianfeng.jiao <jianfeng.jiao@sihuatech.com>
2013-09-23 23:13:16 +0800

    字符串分割

---
+++ src/com/String/SplitTest.java
@@ -0,0 +1,9 @@
+package com.String;
+
+
+public class SplitTest {
+	public static void main(String[] args) {
+		String password = "23455;5423432;42343k;423432;";
+		System.out.println(password.split(";").length);
+	}
+}
